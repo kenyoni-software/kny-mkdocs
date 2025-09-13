2127e7e5f2bc324992c04810c57f36963c186af1
Iceflower <iceflower@gmx.de>
2025-09-13 02:24:01 +0200

    [*] Update developing environment

--- .devcontainer/post-create-command.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-pip install .[dev]
-
-SITE_PACKAGES=$(python -m site --user-site)
-for theme in mkdocs readthedocs; do
-    rm -rf $SITE_PACKAGES/mkdocs/themes/$theme
-    ln -s $SITE_PACKAGES/material/templates $SITE_PACKAGES/mkdocs/themes/$theme
-done
